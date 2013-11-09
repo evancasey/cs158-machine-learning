008ed6e475552febb21f7005f6967d9ef85ba164
Evan Casey <evan.casey92@gmail.com>
2013-11-08 20:43:33 -0800

    more pseudocode

--- python/assignment3.py
+++ python/assignment3.py
@@ -8,33 +8,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def testNeuralNetLearner(data, rounds):
+def testNeuralNetLearner(data, iterations=1000, learn_rate=0.5, momentum=0.1):
 
 		if data == "XOR":
-		    ds = DataSet(name='../data/restaurant', attrnames=attributeNames)
+		    ds = DataSet(name='../data/XOR')
 		    num_input = 4
 		    num_output = 4
 		    num_hl = 1 # number of hidden layers
-		elif data == "Semion":
-		    ds = DataSet(name='../breast-cancer/breast-cancer', attrnames=attributeNames)
+		elif data == "Semeion":
+		    ds = DataSet(name='../data/semeion')
 		    num_input = 256
 		    num_output = 10
 		    num_hl = 1 # number of hidden layers
 		
 		 # transform the ds to matrix
+		 m = _ds_to_matrix(ds)
 		 
-
-		 # create the learner
-		 
+		 # create the learner with the matrix
+		 NNlearner = createNeuralNetLearner(m, num_input, num_output, num_hl, iterations, learn_rate, momentum)
 		 
 		 # test the learner (cross validation)
 		 
 
-def createNeuralNetLearner(m):
+def createNeuralNetLearner(m, num_input, num_output, num_hl, iterations, learn_rate, momentum):
 	 	# create a neural net learner
 
-
 	 	# create weights
+	 	
+
+	 	# call forward propogation
+	 	
+
+	 	# call backward propogation
+	 	
+
+	 	# update weights
+	
+
+def _create_weights(m):
 
 
 def _ds_to_matrix(dataset):
