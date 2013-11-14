f06dc55bffb2464d01ee0643565dd40fad13c661
Evan Casey <evan.casey92@gmail.com>
2013-11-13 17:40:43 -0800

    forward prop finally works

--- python/assignment3.py
+++ python/assignment3.py
@@ -13,8 +13,8 @@ def testNeuralNetLearner(data, iterations=1000, learn_rate=0.5, momentum=0.1):
 
 	if data == "xor":
 	    ds = DataSet(name='../data/xor')
-	    num_input = 4
-	    num_output = 4
+	    num_input = 2
+	    num_output = 2
 	    num_hl = 1 # number of hidden layers
 	elif data == "semeion":
 	    ds = DataSet(name='../data/semeion')
@@ -25,7 +25,7 @@ def testNeuralNetLearner(data, iterations=1000, learn_rate=0.5, momentum=0.1):
 	# create the learner with the matrix
 	NNlearner = createNeuralNetLearner(ds, num_input, num_output, num_hl, iterations, learn_rate, momentum)
  
-	 # test the learner (cross validation)
+	# test the learner (cross validation)
 		 
 
 def createNeuralNetLearner(ds, num_input, num_output, num_hl, iterations, learn_rate, momentum):
@@ -37,18 +37,16 @@ def createNeuralNetLearner(ds, num_input, num_output, num_hl, iterations, learn_
 	mx = np.matrix(inputs)
 
  	# create input weights
- 	mwi = np.random.random((256, 256))
+ 	mwi = np.random.random((num_input + 1, num_input + 1))
 
  	# create output weights
- 	mwo = np.random.random((256, 10))
+ 	mwo = np.random.random((num_input + 1, num_output))
  	
 	# call forward propogation
 	for row in mx:
-		forwardProp(mx,mwi,mwo)
-
-
-
- 	
+		# add in bias column of 1's
+		row = np.append(1,row)
+		forwardProp(np.matrix(row),np.matrix(mwi),np.matrix(mwo))
 
  	# call backward propogation
  	
@@ -56,42 +54,37 @@ def createNeuralNetLearner(ds, num_input, num_output, num_hl, iterations, learn_
  	# update weights
  	
 
-def forwardProp(mx, mwi, mwo):
+def forwardProp(row, mwi, mwo):
 	# takes a list of node values and the associated weights
 	# returns a matrix of updated input and output activations 	
 
-	
 	# 1 dimensional input activation matrix
 	ai = row * mwi
 
 	# initialize matrix to store hidden node values
-	mnh = np.matrix(ai)
+	mnh = ai
 
 	# loop through all of ai and update with sigmoid
-	for i, activation in enumerate(ai):
-		mnh[i] = sigmoid(activation)
+	for i in range(len(ai)):		
+		mnh[i] = sigmoid(ai[0,i])
 
 	# 1 dimensional output activation matrix
 	ao = mnh * mwo
 
-	# initialize matrix to store hidden node values
-	mno = np.matrix(ai)
+	# initialize matrix to store output node values
+	mno = ai
 
-	for i, activation in enumerate(ao):
-		mno[i] = sigmoid(activation)
+	# loop through all of ao and update with sigmoid
+	for i in range(len(ao[0])):	
+		mno[i] = sigmoid(ao[0,i])
 
 	return ai, mnh, mno
-
-	
-
-
-
 
 
 def sigmoid(a):
 	# takes in a value in the activation matrix
 	# returns the result of the sigmoid function performed on it
-	
+
 	return 1/(1 + math.pow(math.e, -a))
 
 
