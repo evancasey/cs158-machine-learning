9875309f4b7ddcd591f2ee083e1f5516805dbc1d
Evan Casey <evan.casey92@gmail.com>
2013-11-18 20:49:00 -0800

    refactor fp, bp, update

--- python/assignment3.py
+++ python/assignment3.py
@@ -5,8 +5,6 @@ import random
 import os
 import sys
 import numpy as np
-# import scipy as sp
-# import matplotlib.pyplot as plt
 
 
 def testNeuralNetLearner(data, iterations=1000, learn_rate=0.1, momentum=0.1):
@@ -14,21 +12,21 @@ def testNeuralNetLearner(data, iterations=1000, learn_rate=0.1, momentum=0.1):
     if data == "xor":
         ds = DataSet(name='../data/xor')
         num_input = 2
-        num_output = 2
+        num_output = 1
         num_hl = 1 # number of hidden layers
     elif data == "semeion":
         ds = DataSet(name='../data/semeion')
         num_input = 256
         num_output = 10
         num_hl = 1 # number of hidden layers
- 
-    for i in range(20):
+
+    for x in range(20):
 
         # create the learner with the matrix
         NNlearner = createNeuralNetLearner(ds, num_input, num_output, num_hl, iterations, learn_rate, momentum)
      
         # create a test observation
-        obs = np.matrix(ds.examples)[0]
+        obs = np.matrix(ds.examples)[1]
 
         # test the learner (cross validation)
         classification = NNlearner(obs)
@@ -41,38 +39,34 @@ def createNeuralNetLearner(ds, num_input, num_output, num_hl, iterations, learn_
 
     # transform the ds to matrix of inputs  
     obs = np.matrix(ds.examples)
-    inputs = np.delete(obs,len(ds.examples) - 2,1)
-    target = obs[:,num_input]
-    mxi = np.matrix(inputs)
-    mxt = np.matrix(target)
+    mx_inputs = np.matrix(np.delete(obs,len(ds.examples) - 2,1))
+    mx_target = obs[:,num_input]
 
     # create input weights
-    mwi = np.random.random((num_input + 1, num_input + 1))
+    mw_input_hidden= np.matrix(np.random.random((num_input + 1, num_input)))
 
     # create output weights
-    mwo = np.random.random((num_input + 1, num_output))
+    mw_hidden_output = np.matrix(np.random.random((num_input + 1, num_output)))
 
+    # repeat for num iterations
     for i in range(iterations):             
     
-        for j in range(len(mxi)):            
+        # go through each observation
+        for j in range(len(mx_inputs)):            
 
             # add in bias column of 1's
-            input_row_with_bias = np.append(1.0,mxi[j])   
+            input_row_with_bias = np.matrix(np.append(1.0,mx_inputs[j]))            
 
             # call forward prop
-            ai, mnh, mno = forwardProp(np.matrix(input_row_with_bias),np.matrix(mwi),np.matrix(mwo))
-
-            # grab target values
-            target_value = mxt[j]
+            mn_hidden, mn_output = forwardProp(input_row_with_bias,mw_input_hidden,mw_hidden_output)            
 
             # call backward prop
-            output_deltas, hidden_deltas = backwardProp(target_value, mno, mnh, mwo, num_output, num_input + 1)
+            hidden_deltas, output_deltas = backwardProp(mx_target[j,0], mn_hidden, mn_output, mw_hidden_output, num_input, num_output)
 
             # update weights
             mwi, mwo = updateWeights(learn_rate, mwi, mwo, hidden_deltas, output_deltas, input_row_with_bias, mnh, mno)
 
             # print "mwi: ", mwi, "\n"
-
 
     def predict(row):
         ai, mnh, mno = forwardProp(row, mwi, mwo)
@@ -81,77 +75,73 @@ def createNeuralNetLearner(ds, num_input, num_output, num_hl, iterations, learn_
     return predict
     
 
-def forwardProp(row, mwi, mwo):
+def forwardProp(row, mw_input_hidden, mw_hidden_output):
     # takes a list of node values and the associated weights
     # returns a matrix of updated input and output activations  
 
-    # print "ROW: ", row, "\n"
-
     # 1 dimensional input activation matrix
-    ai = row * mwi
-
-    # print "AI: ", ai, "\n"
+    a_hidden = row * mw_input_hidden
 
     # initialize matrix to store hidden node values
-    mnh = np.matrix([1.0] * ai.shape[1])
+    mn_hidden = np.matrix([1.0] * a_hidden.shape[1])    
 
-    # loop through all of ai and update with sigmoid
-    for i in range(1, ai.shape[1]):            
-        mnh[0,i] = sigmoid(ai[0,i])
+    # loop through all of hidden activations and update with sigmoid
+    for i in range(a_hidden.shape[1]):            
+        mn_hidden[0,i] = sigmoid(a_hidden[0,i])
 
-    # print "MNH: ", mnh, "\n"
+    # add on bias node to hidden nodes
+    mn_hidden = np.matrix(np.append(1.0,mn_hidden[0]))    
 
     # 1 dimensional output activation matrix
-    ao = mnh * mwo
-
-    # print "AO: ", ao, "\n"
+    a_output = mn_hidden * mw_hidden_output
 
     # initialize matrix to store output node values
-    mno = np.matrix(ao)
+    mn_output = np.matrix([1.0])
 
-    # loop through all of ao and update with sigmoid
-    for i in range(ao.shape[1]):    
-        mno[0,i] = sigmoid(ao[0,i])
+    # loop through all of output activations and update with sigmoid
+    for i in range(a_output.shape[1]):    
+        mn_output[0,i] = sigmoid(a_output[0,i]) 
 
-    # print "MNO: ", mno, "\n"
+    return mn_hidden, mn_output
 
-    return ai, mnh, mno
+def backwardProp(target, mn_hidden, mn_output, mw_hidden_output, num_input, num_output):    
 
-def backwardProp(target, mno, mnh, mwo, num_output, num_hidden):
-
-    # create array of all 0's except 1 for target value
-    desired = [0.0] * num_output
-    desired[target] = 1
+    # create array to represent desired outcome
+    desired = create_desired(target, num_output)
 
     # instantiate array for output deltas
     output_deltas = [0.0] * num_output
 
     # calculating the output deltas
-    for i in range(mno.shape[1]):
-        error = desired[i] - mno[0,i]
-        delta = error * mno[0,i] * (1 - mno[0,i])
-        output_deltas[i] = delta
+    for i in range(mn_output.shape[1]):        
+        error = desired[i] - mn_output[0,i]
+        delta = error * mn_output[0,i] * (1 - mn_output[0,i])
+        output_deltas[i] = delta    
 
     # instantiate array for hidden deltas
-    hidden_deltas = [0.0] * num_hidden
+    hidden_deltas = [0.0] * (num_input + 1)
 
     # calculating the hidden layer deltas
-    for i in range(mnh.shape[1]):
-        weighted_output_sum = 0
+    for i in range(mn_hidden.shape[1]):
+        weighted_output_sum = 0.0
         # sum(weight to output node j * delta of output node j)
         for j in range(len(output_deltas)):
-            weighted_output_sum += (mwo[i,j]*output_deltas[j])
+            weighted_output_sum += (mw_hidden_output[i,j] * output_deltas[j])
 
-        hidden_deltas[i] = mnh[0,i] * (1 - mnh[0,i]) * weighted_output_sum
+        hidden_deltas[i] = mn_hidden[0,i] * (1 - mn_hidden[0,i]) * weighted_output_sum 
 
-    return output_deltas, hidden_deltas
+    # take out the delta for the bias node
+    hidden_deltas = hidden_deltas[1:]
+
+    return hidden_deltas, output_deltas
 
 def updateWeights(learn_rate, mwi, mwo, hidden_deltas, output_deltas, input_row_with_bias, mnh, mno):
     # return updated input and output weights
-    
+
     # update input weights
     for i in range(len(input_row_with_bias)):       
-        for j in range(mnh.shape[1]):
+        for j in range(mnh.shape[1] - 1):
+            # pdb.set_trace()
             mwi[i,j] = mwi[i,j] + (learn_rate * input_row_with_bias[i] * hidden_deltas[j])
 
     # update output weights
@@ -167,6 +157,20 @@ def sigmoid(a):
     # returns the result of the sigmoid function performed on it
 
     return 1/(1 + math.pow(math.e, -a))
+
+def create_desired(target, num_output):
+    # creates an array of binary values to represent 
+    # desired values of the learner
+
+    desired = [0.0] * num_output
+
+    if num_output == 1:
+        # xor
+        desired[0] = target
+    else:
+        desired[target] = 1
+
+    return desired
 
 
 if __name__ == "__main__":
