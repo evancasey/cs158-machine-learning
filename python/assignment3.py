7c515c4f7ed105e88c989b217ad2a4c72a358028
Evan Casey <evan.casey92@gmail.com>
2013-11-19 16:04:08 -0800

    almost there

--- python/assignment3.py
+++ python/assignment3.py
@@ -12,7 +12,7 @@ def testNeuralNetLearner(data, iterations=1000, learn_rate=0.1, momentum=0.1):
     if data == "xor":
         ds = DataSet(name='../data/xor')
         num_input = 2
-        num_output = 1
+        num_output = 2
         num_hl = 1 # number of hidden layers
     elif data == "xorm":
         ds = DataSet(name='../data/xorm')
@@ -30,13 +30,13 @@ def testNeuralNetLearner(data, iterations=1000, learn_rate=0.1, momentum=0.1):
         num_output = 10
         num_hl = 1 # number of hidden layers
 
-    for x in range(1):
+    for x in range(20):
 
         # create the learner with the matrix
         NNlearner = createNeuralNetLearner(ds, num_input, num_output, num_hl, iterations, learn_rate, momentum)
      
         # create a test observation
-        obs = np.matrix(ds.examples)[0]
+        obs = np.matrix(ds.examples)[3]
 
         print obs
 
@@ -60,18 +60,20 @@ def createNeuralNetLearner(ds, num_input, num_output, num_hl, iterations, learn_
     # create output weights
     mw_hidden_output = np.matrix((np.random.random((num_input + 1, num_output)) * 2) - 1)
 
-    print "random mw_input_hidden: ", "\n", mw_input_hidden 
-    print "random mw_hidden_output: ", "\n", mw_hidden_output, "\n"  
+    # print "random mw_input_hidden: ", "\n", mw_input_hidden 
+    # print "random mw_hidden_output: ", "\n", mw_hidden_output, "\n"  
 
     # repeat for num iterations
     for i in range(iterations):             
     
-        print "----"     
+        # print "----"     
 
         # pdb.set_trace()
 
         # go through each observation
         for j in range(len(mx_inputs)):              
+
+            # pdb.set_trace()
 
             # add in bias column of 1's
             input_row_with_bias = np.matrix(np.append(1.0,mx_inputs[j]))           
@@ -79,16 +81,22 @@ def createNeuralNetLearner(ds, num_input, num_output, num_hl, iterations, learn_
             # call forward prop
             mn_hidden, mn_output = forwardProp(input_row_with_bias,mw_input_hidden,mw_hidden_output)       
 
-            print "mn_output: ", "\n", mn_output, "\n"    
+
+            # pdb.set_trace()
+            # print "mn_output: ", "\n", mn_output, "\n"    
 
             # call backward prop
             hidden_deltas, output_deltas = backwardProp(mx_target[j,0], mn_hidden, mn_output, mw_hidden_output, num_input, num_output)
 
+
+            # pdb.set_trace()
             # update weights
             mw_input_hidden, mw_hidden_output = updateWeights(learn_rate, mw_input_hidden, mw_hidden_output, hidden_deltas, output_deltas, input_row_with_bias, mn_hidden, mn_output)
 
-            print "mw_input_hidden: ", "\n", mw_input_hidden 
-            print "mw_hidden_output: ", "\n", mw_hidden_output, "\n"
+            # pdb.set_trace()
+
+            # print "mw_input_hidden: ", "\n", mw_input_hidden 
+            # print "mw_hidden_output: ", "\n", mw_hidden_output, "\n"
 
     def predict(obs):
         mn_hidden, mn_output = forwardProp(obs, mw_input_hidden, mw_hidden_output)
@@ -120,7 +128,7 @@ def forwardProp(row, mw_input_hidden, mw_hidden_output):
     a_output = mn_hidden * mw_hidden_output
 
     # initialize matrix to store output node values
-    mn_output = np.matrix([1.0])
+    mn_output = np.matrix([1.0] * a_output.shape[1])
 
     # loop through all of output activations and update with sigmoid
     for i in range(a_output.shape[1]):    
@@ -150,30 +158,31 @@ def backwardProp(target, mn_hidden, mn_output, mw_hidden_output, num_input, num_
     for i in range(mn_hidden.shape[1]):
         weighted_output_sum = 0.0
         # sum(weight to output node j * delta of output node j)
+        # TODO: this might be taking the wrong length
         for j in range(len(output_deltas)):
             weighted_output_sum += (mw_hidden_output[i,j] * output_deltas[j])
 
-        hidden_deltas[i] = mn_hidden[0,i] * (1 - mn_hidden[0,i]) * weighted_output_sum 
+        hidden_deltas[i] = mn_hidden[0,i] * (1.0 - mn_hidden[0,i]) * weighted_output_sum 
 
     return hidden_deltas, output_deltas
 
+
 def updateWeights(learn_rate, mw_input_hidden, mw_hidden_output, hidden_deltas, output_deltas, input_row_with_bias, mn_hidden, mn_output):
     # return updated input and output weights 
+    
+    # pdb.set_trace()
 
     # update hidden_output weights
     for i in range(mn_hidden.shape[1]):       
         for j in range(mn_output.shape[1]):
             mw_hidden_output[i,j] = mw_hidden_output[i,j] + (learn_rate * mn_hidden[0,i] * output_deltas[j])    
 
-
     # pdb.set_trace()
 
     # update input_hidden weights
     for i in range(input_row_with_bias.shape[1]):       
-        for j in range(mn_hidden.shape[1]):       
-            # pdb.set_trace()     
+        for j in range(mn_hidden.shape[1]):         
             mw_input_hidden[i,j] = mw_input_hidden[i,j] + (learn_rate * input_row_with_bias[0,i] * hidden_deltas[j])
-
 
     # pdb.set_trace()
 
@@ -185,6 +194,7 @@ def sigmoid(a):
     # returns the result of the sigmoid function performed on it
 
     return 1/(1 + math.exp(-a))
+
 
 def create_desired(target, num_output):
     # creates an array of binary values to represent 
@@ -200,7 +210,7 @@ def create_desired(target, num_output):
 
 if __name__ == "__main__":
 
-    testNeuralNetLearner("xor",5,0.1,0.1)
+    testNeuralNetLearner("xor",10000,0.1,0.1)
 
     # do some stuff with the results
 
