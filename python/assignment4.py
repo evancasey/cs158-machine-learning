60286f5c00d727dbcf1d8f314f9c0db953dbdef0
Evan Casey <evan.casey92@gmail.com>
2013-12-05 23:02:56 -0800

    initial commit of assignment 4

---
+++ python/assignment4.py
@@ -0,0 +1,65 @@
+from time import time
+import numpy as np
+import csv
+import pdb
+
+from learning import *
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.preprocessing import normalize
+import pdb
+
+
+def fetchData():
+    ''' Read in the dataset into a ds object '''
+
+    ds = DataSet(name="../data/semeion")
+    return ds
+
+
+def createKMeansLearner(ds,num_clusters=10):
+    ''' Return a kmeans learner trained on ds'''
+
+    kmeans = KMeans(init='k-means++', n_clusters=num_clusters, n_init=10)
+    examples = [row[:-1] for row in ds.examples]
+    norm_examples = normalize(examples).tolist()    
+    kmeans.fit(norm_examples)
+
+    cluster_output_map = getClusterOutputMap(ds,kmeans)
+
+    def predict(obs):
+        cluster = kmeans.predict(obs)
+        return cluster_output_map[cluster]
+
+    return predict
+
+def getClusterOutputMap(ds,learner):
+    ''' Call predict on each row in ds, and return a dictionary
+     to map cluster to digit value'''
+
+    cluster_output_map = {}
+    for row in ds.examples:
+        cluster = learner.predict(row[:-1])[0]        
+        cluster_output_map[cluster] = row[-1]
+
+    return cluster_output_map
+
+
+def testKMeansLearner(ds):
+    ''' Takes a kmeans learner and test data and 
+        runs cross validation '''
+
+    data = fetchData()
+    learner = createKMeansLearner(data)
+    results = cross_validation(learner, ds)
+
+    pdb.set_trace()
+    
+
+if __name__ == '__main__':
+
+    data = fetchData()
+
+    testKMeansLearner(data)
+
+
