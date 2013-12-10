b0722abfde5ff4f0f666078220318173db16babd
Evan Casey <evan.casey92@gmail.com>
2013-12-09 22:43:31 -0800

    pca plots working

--- python/assignment4.py
+++ python/assignment4.py
@@ -4,12 +4,15 @@ import csv
 import pdb
 
 from learning import *
+from collections import defaultdict
 from sklearn.cluster import KMeans
 from sklearn.datasets import load_digits
 from sklearn.preprocessing import scale
 from sklearn.decomposition import PCA
+import random
 import matplotlib.pyplot as pl
 import matplotlib.image as mpimg
+import matplotlib.offsetbox as osb
 import pdb
 
 
@@ -73,22 +76,25 @@ def testKMeansLearner(ds,learner):
     ''' Takes a kmeans learner and test data and 
         runs cross validation '''
 
+    #make copy of original ds
+    pca_ds = copy.deepcopy(ds)
+
     if learner == createPCAKMeansLearner:
         
-        examples = [row[:-1] for row in ds.examples]
+        examples = [row[:-1] for row in pca_ds.examples]
         norm_examples = scale(examples).tolist()    
         reduced_data = PCA(n_components=2).fit_transform(norm_examples)
 
-        reduced_data_with_output = [reduced_data.tolist()[i]+[row[-1]] for i,row in enumerate(ds.examples)]        
+        reduced_data_with_output = [reduced_data.tolist()[i]+[row[-1]] for i,row in enumerate(pca_ds.examples)]        
 
-        ds.examples = reduced_data_with_output
-        ds.target = 2
-        ds.inputs = range(0,2)
+        pca_ds.examples = reduced_data_with_output
+        pca_ds.target = 2
+        pca_ds.inputs = range(0,2)
 
-    return cross_validation(learner, ds) 
+    return cross_validation(learner, pca_ds) 
 
 
-def plotPCAKMeansLearner(ds,learner):
+def plotPCAKMeansLearner(ds,learner,num_clusters=10):
     ''' Takes a kmeans learner and ds, outputs
         a virinoi diagram with each input point plotted '''
 
@@ -98,43 +104,54 @@ def plotPCAKMeansLearner(ds,learner):
     reduced_data = PCA(n_components=2).fit_transform(norm_examples)
     reduced_data_with_output = [reduced_data.tolist()[i]+[row[-1]] for i,row in enumerate(ds.examples)]        
 
-    #instantiate kmeans object
-    kmeans = KMeans(init='k-means++', n_clusters=n_digits, n_init=10)
-    kmeans.fit(reduced_data)
+    #instantiate and fit kmeans object
+    pca_kmeans = KMeans(init='k-means++', n_clusters=num_clusters, n_init=10)
+    pca_kmeans.fit(reduced_data)
 
-    #step size of the mesh
-    h = .02     
-
-    #plot the decision boundary
+    #get the boundaries of our grid
     x_min, x_max = reduced_data[:, 0].min() + 1, reduced_data[:, 0].max() - 1
     y_min, y_max = reduced_data[:, 1].min() + 1, reduced_data[:, 1].max() - 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 
-    #obtain labels for each point in mesh. Use last trained model
-    Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+    #create our background grid
+    plot = pl.subplot(111)
+    pl.xlim([x_min,x_max])
+    pl.ylim([y_min,y_max])
 
-    #put result into color plot
-    Z = Z.reshape(xx.shape)
-    pl.figure(1)
-    pl.clf()
-    pl.imshow(Z, interpolation='nearest',
-              extent=(xx.min(), xx.max(), yy.min(), yy.max()),
-              cmap=pl.cm.Paired,
-              aspect='auto', origin='lower')
+    #create a dictionary of the cluster to img_list
+    img_dict = defaultdict(list)
+    for i,row in enumerate(ds.examples):
 
-    pl.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
-    # Plot the centroids as a white X
-    centroids = kmeans.cluster_centers_
-    pl.scatter(centroids[:, 0], centroids[:, 1],
-               marker='x', s=169, linewidths=3,
-               color='w', zorder=10)
-    pl.title('K-means clustering on the digits dataset (PCA-reduced data)\n'
-             'Centroids are marked with white cross')
-    pl.xlim(x_min, x_max)
-    pl.ylim(y_min, y_max)
-    pl.xticks(())
-    pl.yticks(())
+        #get the cluster
+        cluster = pca_kmeans.labels_[i]
+
+        #get the img and img coordinates
+        coordinates = reduced_data[i]
+        img = np.array(ds.examples[i][:-1]).reshape((16,16))
+
+        #for each cluster append the img to its value
+        img_dict[cluster].append((img,coordinates))
+
+    #plot a subset of images for each cluster
+    for k,v in img_dict.items():
+
+        #sample 10 images
+        img_list = random.sample(v,10)
+
+        #plot each subset 
+        for i,img_and_coord in enumerate(v):            
+            if i < 10:
+                osb_img = osb.OffsetImage(img_list[i][0],zoom=1.5)
+                xy = img_list[i][1].tolist()
+
+                ab = osb.AnnotationBbox(osb_img,xy,xycoords='data')                                  
+                plot.add_artist(ab)
+
+    #draw and show the plot
+    pl.draw()
     pl.show()
+
+    #TODO: get rid of border, make background black, change colors...
+
 
 
 if __name__ == '__main__':
@@ -144,8 +161,8 @@ if __name__ == '__main__':
     # results = testKMeansLearner(data,createKMeansLearner)
     # print "Normal Kmeans: ", results
 
-    results = testKMeansLearner(data,createPCAKMeansLearner)
-    print "PCA Kmeans: ", results
+    # results = testKMeansLearner(data,createPCAKMeansLearner)
+    # print "PCA Kmeans: ", results
 
     plotPCAKMeansLearner(data,createPCAKMeansLearner)
 
